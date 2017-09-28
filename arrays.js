991a2aec8e4209639df2b09a99a76c47f44b1675
Julee117 <juliannne@gmail.com>
2017-09-28 18:56:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  // var newArray = array.slice();
-  // newArray.unshift(element);
-  // return newArray;
   return [element, ...array];
 }
 
@@ -13,9 +10,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = array.slice();
-  newArray.push(element);
-  return newArray;
+  // var newArray = array.slice();
+  // newArray.push(element);
+  // return newArray;
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
